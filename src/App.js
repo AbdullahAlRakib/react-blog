b2d896e00a430ac28fc0e23d2b582a198fd7b0e2
AbdullahAlRakib <rakibdev76@gmail.com>
2022-04-01 15:17:08 +0600

    add-navbar and home components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 function App() {
-  const title="Welcome to the blog";
+
   return (
     <div className="App">
+      <Navbar/>
      <div className='content'>
-       <h1>{title}</h1>
+       <Home/>
      </div>
     </div>
   );
