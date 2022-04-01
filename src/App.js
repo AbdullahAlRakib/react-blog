6b252865ab63e172b5676fe419c86c58b0504108
AbdullahAlRakib <rakibdev76@gmail.com>
2022-04-01 15:10:06 +0600

    add dynamic value in react templates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
 import './App.css';
 function App() {
+  const title="Welcome to the blog";
   return (
     <div className="App">
      <div className='content'>
-       <h1>Hello World ,App Component</h1>
+       <h1>{title}</h1>
      </div>
     </div>
   );
